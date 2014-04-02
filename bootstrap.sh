0e3e165c789769632d91db5a4d4f29caeeb6f200
Sebastian Ziebell <sebastian.ziebell@asquera.de>
2014-04-02 21:50:31 +0200

    Shell reload maps to zsh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ git pull origin master
 function doIt() {
   rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
     --exclude "README.md" --exclude "LICENSE-MIT.txt" -av --no-perms . ~
-  source ~/.bash_profile
+  source ~/.zshrc
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
   doIt
